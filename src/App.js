0a2d9a0285773903e54bc08fb9e6f9be89e89924
mmmccormack <pmccorma@gmail.com>
2021-04-10 14:24:56 -0400

    added firebase integration - can add remove and mark stuff as viewed! :clap::clap:

--- src/App.js
+++ src/App.js
@@ -1,18 +1,97 @@
-// import firebase from './firebase';
+import firebase from './firebase';
 import { useState, useEffect } from 'react';
 import RecommendationsList from './RecommendationsList';
 import './App.css';
-
 function App() {
 
   // setting up our method to set state. Has to be an empty array because of the .map method in our code below - it can map through an empty array and return nothing, but if it was a different data type, that would cause .map to not function!
   const [list, setList] = useState([]);
-  // because you only want to call the API once when the component renders, call the useEffect function with an empty dependency array at the end.
-  useEffect( () => {
+  // this will for the text input
+  const [userInput, setUserInput] = useState('');
+  // this is for the dropdown
+  const [mediaType, setMediaType] = useState('');
+  // set saved list state for firebase
+  const [savedList, setSavedList] = useState([]);
+
+
+
+    // firebase integrations
+    const fakeResults = [
+      {Name : "Mad Men", Type : "show"},
+      {Name : "The Wire", Type : "show"},
+      {Name : "The Terror", Type : "show"},
+    ]
+  
+    useEffect( () => {
+      // establish connection to firebase DB
+      const dbRef = firebase.database().ref();
+      // add event listener for any time firebase DB changes.
+      dbRef.on('value', (res) => {
+        // create new variable to store the new state that we want to introduce to our app
+        const newState = [];
+        // use res.val to store the response from your DB in a useful object chunk
+        const data = res.val();
+        // iterate using for in loop to get books into array, and to have unique key value assigned to each one
+        for (let key in data) {
+          newState.push({
+            key : key,
+            name : data[key],
+          })
+        }
+        // call setBooks function to update out component's state to be the new value
+        setSavedList(newState);
+      })
+    }, [])
+
+    const handleRemove = (itemId) => {
+      const dbRef = firebase.database().ref();
+      dbRef.child(itemId).remove();
+    }
+
+    const handleViewed = (item) => {
+      // const dbRef = firebase.database().ref(`${item.key}`);
+      
+      // const properties = {
+      //   title : item.name.title,
+      //   type : item.name.type,
+      //   viewed : true
+      // }
+      // dbRef.set(properties);
+
+      const dbRef = firebase.database().ref(`${item.key}`);
+      
+      const properties = {};
+
+      if (item.name.viewed) {
+        properties.viewed = false;
+      } else {
+        properties.viewed = true;
+        
+      }
+      
+      dbRef.update(properties);
+      
+      
+    }
+
+
+
+
+
+  // this handles the change of the user text input
+  const handleTextChange = (event) => {
+    setUserInput(event.target.value);
+  }
+  const handleSelectChange = (event) => {
+    setMediaType(event.target.value);
+    // console.log(event.target.value);
+  }
+  const formSubmit = (event) => {
+    event.preventDefault();
     const url = new URL(`https://proxy.hackeryou.com`)
         url.search = new URLSearchParams({
             reqUrl : `https://tastedive.com/api/similar`,
-            'params[q]' : `book:trainspotting`,
+            'params[q]' : `${mediaType}:${userInput}`,
             'params[k]' : `407899-PatrickM-CM8VF4NQ`,
             'params[info]' : 1,
             'params[limit]' : 15
@@ -28,11 +107,51 @@ function App() {
                 setList(apiJson.Similar.Results);
               }
             })
-      }, []);
-
+  }
 
   return (
     <div className="App">
+
+
+      <ul>
+        {
+          savedList.map( (item) => {
+            return (
+              <li key={item.key}>
+                <p>{item.name.title} is a show. </p>
+                <button onClick={ () => { handleViewed(item) } }>Viewed</button>
+                <button onClick={ () => { handleRemove(item.key) } }>Remove</button>
+              </li>
+            )
+          }) 
+        }
+      </ul>
+
+
+
+      {/* form to add new book to list */}
+      <form action="submit">
+        <label htmlFor="media">Enter the name of what you would like to search for</label>
+        <input 
+          type="text" 
+          id="media"
+          onChange={ handleTextChange }
+          value={ userInput }
+          />
+          <select 
+            name="mediaType" 
+            id="mediaType"
+            onChange={ handleSelectChange }>
+            <option value="">Select Media Type</option>
+            <option value="band">Music</option>
+            <option value="movie">Movie</option>
+            <option value="show">TV Show</option>
+            <option value="podcast">Podcast</option>
+            <option value="book">Book</option>
+            <option value="game">Game</option>
+          </select>
+          <button onClick={ formSubmit }>Submit</button>
+      </form>
       <ul>
         {/* send results from API call as props to list component */}
         {
@@ -43,6 +162,7 @@ function App() {
                 name={listItem.Name}
                 type={listItem.Type}
               />
+              
             )
           })
         }
@@ -50,5 +170,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
--- src/RecommendationsList.js
+++ src/RecommendationsList.js
@@ -1,9 +1,22 @@
-
+import firebase from './firebase';
 // you need to have props in your parameter in order to pass info from the props object in your App.js to your component.
 const RecommendationsList = ( props ) => {
 
+    const handleClick = (e) => {
+        e.preventDefault();
+        const dbRef = firebase.database().ref();
+        const properties = {
+            title : props.name,
+            type : props.type,
+            viewed : false
+        }
+        dbRef.push(properties);
+        // dbRef.push(props.name);
+      }
+
+
     return(
-            <li>{props.type} - {props.name}</li>
+            <li>{props.type} - {props.name} <button onClick={ handleClick }>Add book</button></li> 
     )
 }
 
