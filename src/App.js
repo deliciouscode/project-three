d463bc9bb538b195842b805272a7d6cb34e77e13
Kornthip <sang.kornthip@gmail.com>
2021-04-10 13:06:48 -0400

    form and results ready, start Sass:v:

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 // import firebase from './firebase';
 import { useState, useEffect } from 'react';
 import RecommendationsList from './RecommendationsList';
-import './App.css';
+import Footer from './Footer';
+import './App.scss';
 
 function App() {
-
   // setting up our method to set state. Has to be an empty array because of the .map method in our code below - it can map through an empty array and return nothing, but if it was a different data type, that would cause .map to not function!
   const [list, setList] = useState([]);
-  // because you only want to call the API once when the component renders, call the useEffect function with an empty dependency array at the end.
-  useEffect( () => {
+  // this will for the text input
+  const [userInput, setUserInput] = useState('');
+  // this is for the dropdown
+  const [mediaType, setMediaType] = useState('');
+  // this handles the change of the user text input
+  const handleTextChange = (event) => {
+    setUserInput(event.target.value);
+  }
+  const handleSelectChange = (event) => {
+    setMediaType(event.target.value);
+  }
+  const formSubmit = (event) => {
+    event.preventDefault();
     const url = new URL(`https://proxy.hackeryou.com`)
         url.search = new URLSearchParams({
             reqUrl : `https://tastedive.com/api/similar`,
-            'params[q]' : `book:trainspotting`,
+            'params[q]' : userInput,
+            'params[type]' : mediaType,
             'params[k]' : `407899-PatrickM-CM8VF4NQ`,
             'params[info]' : 1,
             'params[limit]' : 15
@@ -27,15 +39,42 @@ function App() {
               } else {
                 setList(apiJson.Similar.Results);
               }
+            }).catch(()=>{
+              console.log('fetch error');
             })
-      }, []);
-
-
+  }
+ 
   return (
     <div className="App">
+      {/* form to add new book to list */}
+      <form action="submit">
+        <label htmlFor="media">Enter the name of what you would like to search for</label>
+        <input 
+          type="text" 
+          id="media"
+          onChange={ handleTextChange }
+          value={ userInput }
+          required="required"
+          />
+          <select 
+            name="mediaType" 
+            id="mediaType"
+            onChange={ handleSelectChange }
+            value={ mediaType }>
+            <option value="">Select Media Type</option>
+            <option value="band">Music</option>
+            <option value="movie">Movie</option>
+            <option value="show">TV Show</option>
+            <option value="podcast">Podcast</option>
+            <option value="book">Book</option>
+            <option value="game">Game</option>
+          </select>
+          <button onClick={ formSubmit }>Submit</button>
+      </form>
       <ul>
         {/* send results from API call as props to list component */}
         {
+
           list.map( (listItem, index) => {
             return(
               <RecommendationsList
@@ -45,10 +84,12 @@ function App() {
               />
             )
           })
+          
         }
       </ul>
+
+      <Footer />
     </div>
   );
 }
-
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return(
+        <footer>
+            <p>created by <a href="https://kornthip.me/">Kornthip Sangsuriyachot</a> and <a href="https://mmmccormack.com/">Patrick McCormack</a> @ <a href="https://junocollege.com/">Juno College</a></p>
+        </footer>
+    )
+}
+
+export default Footer;
