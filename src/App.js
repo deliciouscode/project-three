47cb495b8a4be303bafa95697595b8303d451c95
mmmccormack <pmccorma@gmail.com>
2021-04-10 19:40:24 -0400

    cleaned up some :fire:base code and added pseudocode :star2:

--- src/App.js
+++ src/App.js
@@ -14,14 +14,7 @@ function App() {
   const [savedList, setSavedList] = useState([]);
 
 
-
-    // firebase integrations
-    const fakeResults = [
-      {Name : "Mad Men", Type : "show"},
-      {Name : "The Wire", Type : "show"},
-      {Name : "The Terror", Type : "show"},
-    ]
-  
+  // this useEffect is added for establishing a connection to the firebase database anytime the user opens the app or makes changes to the database
     useEffect( () => {
       // establish connection to firebase DB
       const dbRef = firebase.database().ref();
@@ -31,94 +24,86 @@ function App() {
         const newState = [];
         // use res.val to store the response from your DB in a useful object chunk
         const data = res.val();
-        // iterate using for in loop to get books into array, and to have unique key value assigned to each one
+        // iterate using for in loop to get items into array, and to have unique key value assigned to each one
         for (let key in data) {
           newState.push({
             key : key,
             name : data[key],
           })
         }
-        // call setBooks function to update out component's state to be the new value
+        // call this function to update out component's state to be the new value
         setSavedList(newState);
       })
     }, [])
-
+    
+    // this function handles removing saved items from the database
     const handleRemove = (itemId) => {
       const dbRef = firebase.database().ref();
       dbRef.child(itemId).remove();
     }
 
+    // this function handles changing the status of a saved item from viewed to not yet viewed
     const handleViewed = (item) => {
-      // const dbRef = firebase.database().ref(`${item.key}`);
-      
-      // const properties = {
-      //   title : item.name.title,
-      //   type : item.name.type,
-      //   viewed : true
-      // }
-      // dbRef.set(properties);
-
+      // establish firebase connection specific to the item referred to by the user
       const dbRef = firebase.database().ref(`${item.key}`);
-      
+      // create an empty object to update the viewed property
       const properties = {};
-
+      // conditional to toggle the database viewed value between true and false 
       if (item.name.viewed) {
         properties.viewed = false;
       } else {
         properties.viewed = true;
-        
       }
-      
+      // update the database with the new value of the viewed vaue
       dbRef.update(properties);
-      
-      
     }
-
-
-
 
 
   // this handles the change of the user text input
   const handleTextChange = (event) => {
     setUserInput(event.target.value);
   }
+
+  // this handles the change of the dropdown menu
   const handleSelectChange = (event) => {
     setMediaType(event.target.value);
-    // console.log(event.target.value);
   }
+
+  // this function gathers the input from the text box and the dropdown menu and sends them to the API in order to set the state in the RecommendationsList component
   const formSubmit = (event) => {
     event.preventDefault();
     const url = new URL(`https://proxy.hackeryou.com`)
         url.search = new URLSearchParams({
             reqUrl : `https://tastedive.com/api/similar`,
-            'params[q]' : `${mediaType}:${userInput}`,
+            'params[q]' : userInput,
+            'params[type]' : mediaType,
             'params[k]' : `407899-PatrickM-CM8VF4NQ`,
             'params[info]' : 1,
             'params[limit]' : 15
         });
-        fetch(url)
-            .then( res => res.json())
-            // this set method will completely replace the useState array with our array of results
-            .then( apiJson => {
-            // this statement handles any possible error handling in case something is not found.
-              if (apiJson.Similar.Results.length === 0) {
-                console.log('nothing found!');
-              } else {
-                setList(apiJson.Similar.Results);
-              }
-            })
+    fetch(url)
+        .then( res => res.json())
+        // this set method will completely replace the useState array with our array of results
+        .then( apiJson => {
+        // this statement handles any possible error handling in case something is not found.
+          if (apiJson.Similar.Results.length === 0) {
+            console.log('nothing found!');
+          } else {
+            setList(apiJson.Similar.Results);
+          }
+        });
   }
 
   return (
     <div className="App">
-
-
+    {/* list of items saved by the user */}
       <ul>
         {
           savedList.map( (item) => {
             return (
               <li key={item.key}>
-                <p>{item.name.title} is a show. </p>
+                {/* added ternary operator to denote if a user has viewed the saved item or not */}
+                {item.name.viewed ? <p><strong>{item.name.title} has been viewed.</strong></p> : <p>{item.name.title}</p> }
                 <button onClick={ () => { handleViewed(item) } }>Viewed</button>
                 <button onClick={ () => { handleRemove(item.key) } }>Remove</button>
               </li>
@@ -129,7 +114,7 @@ function App() {
 
 
 
-      {/* form to add new book to list */}
+      {/* form to call API for recommendations */}
       <form action="submit">
         <label htmlFor="media">Enter the name of what you would like to search for</label>
         <input 
@@ -137,20 +122,20 @@ function App() {
           id="media"
           onChange={ handleTextChange }
           value={ userInput }
-          />
-          <select 
-            name="mediaType" 
-            id="mediaType"
-            onChange={ handleSelectChange }>
-            <option value="">Select Media Type</option>
-            <option value="band">Music</option>
-            <option value="movie">Movie</option>
-            <option value="show">TV Show</option>
-            <option value="podcast">Podcast</option>
-            <option value="book">Book</option>
-            <option value="game">Game</option>
-          </select>
-          <button onClick={ formSubmit }>Submit</button>
+        />
+        <select 
+          name="mediaType" 
+          id="mediaType"
+          onChange={ handleSelectChange }>
+          <option value="">Select Media Type</option>
+          <option value="band">Music</option>
+          <option value="movie">Movie</option>
+          <option value="show">TV Show</option>
+          <option value="podcast">Podcast</option>
+          <option value="book">Book</option>
+          <option value="game">Game</option>
+        </select>
+        <button onClick={ formSubmit }>Submit</button>
       </form>
       <ul>
         {/* send results from API call as props to list component */}
@@ -162,7 +147,6 @@ function App() {
                 name={listItem.Name}
                 type={listItem.Type}
               />
-              
             )
           })
         }
