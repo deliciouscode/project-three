36b6954c4eefa56cb03d841aa83e74055b418cf2
mmmccormack <pmccorma@gmail.com>
2021-04-11 14:55:00 -0400

    added icons, error handling, and better functionality! :v::v:

--- src/RecommendationsList.js
+++ src/RecommendationsList.js
@@ -1,4 +1,10 @@
 import firebase from './firebase';
+import music from './assets/music.svg'
+import show from './assets/show.svg'
+import podcast from './assets/podcast.svg'
+import book from './assets/book.svg'
+import movie from './assets/movie.svg'
+import game from './assets/game.svg'
 // you need to have props in your parameter in order to pass info from the props object in your App.js to your component.
 const RecommendationsList = ( props ) => {
 
@@ -8,15 +14,31 @@ const RecommendationsList = ( props ) => {
         const properties = {
             title : props.name,
             type : props.type,
-            viewed : false
+            notviewed : true
         }
         dbRef.push(properties);
-        // dbRef.push(props.name);
+        
       }
 
+    let image;
+    if (props.type === 'music') {
+        image = music;
+    } else if (props.type === 'show') {
+        image = show;
+    } else if (props.type === 'podcast') {
+        image = podcast;
+    } else if (props.type === 'book') {
+        image = book;
+    } else if (props.type === 'movie') {
+        image = movie;
+    } else if (props.type === 'game') {
+        image = game;
+    }
 
     return(
-            <li>{props.type} - {props.name} <button onClick={ handleClick }>Add book</button></li> 
+            <li> 
+                <img src={image} alt={props.type}/>{props.name} 
+                <button onClick={ handleClick }>Save to List</button></li> 
     )
 }
 
